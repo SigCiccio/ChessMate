9e3f6f18acd29db72cefcbc7f7d4c5b0ac50a376
SigCiccio <mailpercoseacaso9909@gmail.com>
2024-01-12 09:01:17 +0100

    Modifyed

--- utils/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-session_start();
-
-require_once("utils/functions.php");
-require_once("utils/DatabaseHelper.php");
-
-$dbh = new DatabaseHelper("localhost", "root", "", "chessmate", 3306);
